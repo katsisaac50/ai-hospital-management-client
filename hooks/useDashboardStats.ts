8ee1cabfa2f447a641999ce8cca79a2f64df8b45
katsisaac50 <katsisaac50@gmail.com>
2025-08-08 10:08:30 +0200

    swr error fixed

--- hooks/useDashboardStats.ts
+++ hooks/useDashboardStats.ts
@@ -2,7 +2,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import useSWR from "swr"
+import useSWR from 'swr'
 import axios from "axios"
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data)
